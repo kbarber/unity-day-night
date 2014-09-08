4da6e8417b393ca14ce99f5396af4a963a0a4b9f
Ken Barber <ken@bob.sh>
2014-09-08 01:47:51 +0100

    Missing files from last commit
    Signed-off-by: Ken Barber <ken@bob.sh>

---
+++ Assets/Lib/Scripts/LightAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("DN/Light Action")]
+public class LightAction : MonoBehaviour
+{
+	// Source of light
+	public Light lightSource;
+
+	// RO: Light off or on?
+	public bool lightOn = true;
+
+	// Turn off/on light
+	void doAction ()
+	{
+		print ("Light Action");
+		if (lightOn) {
+			lightSource.enabled = false;
+			lightOn = false;
+		} else {
+			lightSource.enabled = true;
+			lightOn = true;
+		}
+	}
+}

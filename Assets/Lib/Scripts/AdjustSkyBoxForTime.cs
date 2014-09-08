be25f2f2b7c5a373bf701d87eb030398d5f81753
Ken Barber <ken@bob.sh>
2014-09-08 04:32:31 +0100

    Add streetlights and road, adjust color of sky based on time
    Signed-off-by: Ken Barber <ken@bob.sh>

---
+++ Assets/Lib/Scripts/AdjustSkyBoxForTime.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustSkyBoxForTime : MonoBehaviour
+{
+	public RotateUniverse rotateUniverse;
+	public Camera cameraToAdjust;
+	public Color dayColor = new Color (135, 206, 235);
+	public Color nightColor = new Color (0, 0, 0);
+
+	private Color SkyBoxColor (bool day)
+	{
+		if (day) {
+			return dayColor;
+		} else {
+			return nightColor;
+		}
+	}
+
+	void updateSkyBox ()
+	{
+		cameraToAdjust.backgroundColor = SkyBoxColor (rotateUniverse.isDay);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		updateSkyBox ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		updateSkyBox ();
+	}
+}
--- Assets/Lib/Scripts/RotateUniverse.cs
+++ Assets/Lib/Scripts/RotateUniverse.cs
@@ -2,14 +2,59 @@
 using System.Collections;
 
 [AddComponentMenu("DN/Rotate Universe")]
-public class RotateUniverse : MonoBehaviour {
+public class RotateUniverse : MonoBehaviour
+{
+	// How many degrees per second to move the sun & moon
+	public int degreesPerSecond = 10;
 
-	void Rotate () {
-		// Slowly rotate the object around its X axis at 1 degree/second.
-		transform.Rotate(0, 0, Time.deltaTime);
+	// Time of the world, from 0 to maximumTime
+	public float worldTime;
+
+	// Maximum time
+	public float maximumWorldTime = 100;
+
+	// Starting Position
+	public float startingTime = 0.25f;
+
+	// Day?
+	public bool isDay;
+
+	private void Rotate ()
+	{
+		// Rotate the object around its X axis at 10 degree/second.
+		transform.Rotate (0, 0, Time.deltaTime * degreesPerSecond);
 	}
 
-	void Update () {
-		Rotate();
+	/* Here we convert the rotation of the world into time */
+	private static float RotationToWorldTime (float eulerAngle, float maxTime, float startTime)
+	{
+		float rotationCalc = eulerAngle / 360 * maxTime;
+		return ((maxTime * startTime) + rotationCalc) % maxTime;
+	}
+
+	private static bool calcDay (float currentTime, float maxTime)
+	{
+		if (currentTime < (maxTime / 2)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void UpdateWorldTime ()
+	{
+		worldTime = RotationToWorldTime (transform.rotation.eulerAngles.z, maximumWorldTime, startingTime);
+		isDay = calcDay (worldTime, maximumWorldTime);
+	}
+
+	void Start ()
+	{
+		UpdateWorldTime ();
+	}
+
+	void Update ()
+	{
+		Rotate ();
+		UpdateWorldTime ();
 	}
 }

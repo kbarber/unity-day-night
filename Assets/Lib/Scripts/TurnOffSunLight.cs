6a0123872fd523f2b03dce2aa5683c2c8eb5e726
Ken Barber <ken@bob.sh>
2014-09-08 04:48:25 +0100

    Include sun off/on script
    Signed-off-by: Ken Barber <ken@bob.sh>

---
+++ Assets/Lib/Scripts/TurnOffSunLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOffSunLight : MonoBehaviour
+{
+	public Light sunLight;
+	public RotateUniverse rotateUniverse;
+	
+	private void adjustSunlight ()
+	{
+		if (rotateUniverse.isDay) {
+			sunLight.enabled = true;
+		} else {
+			sunLight.enabled = false;
+		}
+	}
+
+	void Start ()
+	{
+		adjustSunlight ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		adjustSunlight ();
+	}
+}

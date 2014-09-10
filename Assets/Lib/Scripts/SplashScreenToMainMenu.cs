e19d6dda56678d9982d8389fb5e191c358894d6e
Ken Barber <ken@bob.sh>
2014-09-10 03:15:22 +0100

    Convert SplashScreenToMainMenu from js to cs
    Signed-off-by: Ken Barber <ken@bob.sh>

---
+++ Assets/Lib/Scripts/SplashScreenToMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("DN/Splash Screen To Main Menu")]
+[RequireComponent (typeof(GUITexture))]
+public class SplashScreenToMainMenu : MonoBehaviour
+{
+
+	public float timeToWait = 3.0f;
+
+	IEnumerator WaitCoroutine ()
+	{
+		yield return new WaitForSeconds (timeToWait);
+		Application.LoadLevel ("MainMenu");
+	}
+
+	void Start ()
+	{
+		StartCoroutine (WaitCoroutine ());
+	}
+}

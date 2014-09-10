028161d9502ccc0aa72bfe8f1c451e9a8c023e7f
Ken Barber <ken@bob.sh>
2014-09-10 19:10:37 +0100

    Add starry night sky box
    Signed-off-by: Ken Barber <ken@bob.sh>

--- Assets/Lib/Scripts/AdjustSkyBoxForTime.cs
+++ Assets/Lib/Scripts/AdjustSkyBoxForTime.cs
@@ -7,6 +7,8 @@ public class AdjustSkyBoxForTime : MonoBehaviour
 	public Camera cameraToAdjust;
 	public Color dayColor = new Color (135, 206, 235);
 	public Color nightColor = new Color (0, 0, 0);
+	public Material nightSkybox;
+	public Material daySkybox;
 
 	private Color SkyBoxColor (bool day)
 	{
@@ -17,9 +19,22 @@ public class AdjustSkyBoxForTime : MonoBehaviour
 		}
 	}
 
+	private Material SkyboxMaterial (bool day)
+	{
+		if (day) {
+			return daySkybox;
+		} else {
+			return nightSkybox;
+		}
+	}
+
 	void updateSkyBox ()
 	{
 		cameraToAdjust.backgroundColor = SkyBoxColor (rotateUniverse.isDay);
+		Skybox skybox = cameraToAdjust.GetComponent<Skybox> ();
+		if (skybox != null) {
+			skybox.material = SkyboxMaterial (rotateUniverse.isDay);
+		}
 	}
 
 	// Use this for initialization

383f688996d94d20f4158e4864f0bb96f8203ac8
Ken Barber <ken@bob.sh>
2014-09-14 02:27:25 +0100

    Use new script SwitchToSceneAction for switching to/from space and the spaceport
    Signed-off-by: Ken Barber <ken@bob.sh>

---
+++ Assets/Lib/Scripts/SwitchToSceneAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("DN/Switch To Scene Action")]
+public class SwitchToSceneAction : MonoBehaviour
+{
+	public string sceneName;
+
+	void doAction ()
+	{
+		Application.LoadLevel (sceneName);
+	}
+}

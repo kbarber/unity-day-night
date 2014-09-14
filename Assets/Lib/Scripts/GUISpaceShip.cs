7503a471db9e35a6c123746133ce62b287692d6c
Ken Barber <ken@bob.sh>
2014-09-15 00:35:08 +0100

    Add ship power controls
    Signed-off-by: Ken Barber <ken@bob.sh>

---
+++ Assets/Lib/Scripts/GUISpaceShip.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUISpaceShip : MonoBehaviour
+{
+
+	public int enginePower = 0;
+
+	void OnGUI ()
+	{
+		float axis = Input.GetAxis ("Vertical");
+		if ((axis > 0) && enginePower < 100) {
+			enginePower++;
+		} else if ((axis < 0) && enginePower > 0) {
+			enginePower--;
+		}
+		GUI.Box (new Rect (10, 45, 120, 25), "Engine Power: " + enginePower);
+	}
+}
